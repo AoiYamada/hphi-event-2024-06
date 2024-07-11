496a40b7398c2f4ca8d52135520bc88c3dabc2ec
Pan <kpkong@hk01.com>
2024-07-12 03:45:57 +0800

    fix: width

--- src/components/sections/Students.tsx
+++ src/components/sections/Students.tsx
@@ -8,7 +8,7 @@ const Students = () => {
         <h1 className="flex flex-col items-center justify-center text-3xl font-semibold sm:flex-row sm:text-4xl lg:text-5xl">
           學員分享
         </h1>
-        <Sharing className="w-sm max-w-md" />
+        <Sharing className="w-sm max-w-full md:max-w-md" />
       </MaxWidthWrapper>
     </section>
   );
