30439e5555c6efcce749f11ab4213fc2c9de19b4
Pan <kpkong@hk01.com>
2024-07-19 23:37:54 +0800

    feat: add phone

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -61,7 +61,12 @@ const contacts: {
   },
   {
     icon: <Phone />,
-    name: "35007168",
+    name: "35007168(熱線)",
+    link: "tel:35007168",
+  },
+  {
+    icon: <Phone />,
+    name: "26261828(催眠課程專線)",
     link: "tel:35007168",
   },
 ];
