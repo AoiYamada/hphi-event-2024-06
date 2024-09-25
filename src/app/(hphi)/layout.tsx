e9cd1a0f199cd78d26e0126c69bbc72134ae6eab
Pan Kong <empty@github.com>
2024-09-26 03:11:43 +0800

    fix: style path

--- src/app/(hphi)/layout.tsx
+++ src/app/(hphi)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { cn } from "@/lib/utils";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
