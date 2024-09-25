8ec39cd734b3ee7f31541cdbe612e650fdcc9635
Pan Kong <empty@github.com>
2024-09-26 03:07:38 +0800

    refactor: restructure

--- src/app/(hphi)/page.tsx
+++ src/app/(hphi)/page.tsx
@@ -1,14 +1,14 @@
 import { Toaster } from "@/components/ui/toaster";
-import SignUp from "../components/SignUp";
-import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
-import ContactUs from "../components/sections/ContactUs";
-import CourseInfo from "../components/sections/CourseInfo";
-import Hero from "../components/sections/Hero";
-import RecAsso from "../components/sections/RecAsso";
-import TimeSlot from "../components/sections/TimeSlot";
-import Students from "../components/sections/Students";
-import Tutors from "../components/sections/Tutors";
+import SignUp from "@/components/SignUp";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import ContactUs from "@/components/sections/ContactUs";
+import CourseInfo from "@/components/sections/CourseInfo";
+import Hero from "@/components/sections/Hero";
+import RecAsso from "@/components/sections/RecAsso";
+import TimeSlot from "@/components/sections/TimeSlot";
+import Students from "@/components/sections/Students";
+import Tutors from "@/components/sections/Tutors";
 import HypnosisIntroVideo from "@/components/HypnosisIntroVideo";
 
 function Home() {
---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Private",
+  description: "",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body id="root" className={cn(inter.className)}>
+        {children}
+      </body>
+    </html>
+  );
+}
